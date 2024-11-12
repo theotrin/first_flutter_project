05844ec7ccd41ec1d00c63244150d89d44d595f7
Theo Pinheiro <19900@bemol.local>
2024-11-12 11:08:23 -0400

    feat: adcionado mais componentes na tela!

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,85 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Stack(
-        children: [
-          Container(color: Colors.blue,),
-          Container(color: Colors.amber,width: 100,height: 100,),
-        ],
-      ),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Container(
+            color: Colors.white,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Stack(alignment: Alignment.center, children: [
+                  Container(color: Colors.red, width: 150, height: 150),
+                  Container(
+                    color: Colors.blue,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.amber,
+                    width: 50,
+                    height: 50,
+                  ),
+                ]),
+                Stack(alignment: Alignment.center, children: [
+                  Container(color: Colors.red, width: 150, height: 150),
+                  Container(
+                    color: Colors.blue,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.amber,
+                    width: 50,
+                    height: 50,
+                  ),
+                ]),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Container(
+                    color: Colors.pink,
+                    width: 50,
+                    height: 50,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    width: 50,
+                    height: 50,
+                  ),
+                  Container(
+                    color: Colors.pink,
+                    width: 50,
+                    height: 50,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    width: 50,
+                    height: 50,
+                  ),
+                ]),
+                Container(
+                  color: Colors.amber,
+                  height: 30,
+                  width: 300,
+                  child: Text(
+                    'Diamante amarelo',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print('Você apertou o botão');
+                  },
+                  child: Text('Aperte o botão!'),
+                )
+              ],
+            )));
   }
 }
